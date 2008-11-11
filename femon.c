fa41c95b1c16f6d3845f8fecc29b42a43d272f78
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2008-11-11 16:30:21 +0200

    Updated Italian translation (Thanks to Diego Pierotto).

--- femon.c
+++ femon.c
@@ -18,7 +18,7 @@
 #error "VDR-1.6.0 API version or greater is required!"
 #endif
 
-static const char VERSION[]       = "1.6.3";
+static const char VERSION[]       = "1.6.4";
 static const char DESCRIPTION[]   = trNOOP("DVB Signal Information Monitor (OSD)");
 static const char MAINMENUENTRY[] = trNOOP("Signal Information");
 
