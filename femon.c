ee43f5936f24d2ecb94f41bc6ee4186bcd81a09f
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2013-03-09 13:19:03 +0200

    Updated French translation (Thanks to Bernard Jaulin).

--- femon.c
+++ femon.c
@@ -22,7 +22,7 @@
 #define GITVERSION ""
 #endif
 
-static const char VERSION[]       = "1.7.18" GITVERSION;
+static const char VERSION[]       = "1.7.19" GITVERSION;
 static const char DESCRIPTION[]   = trNOOP("DVB Signal Information Monitor (OSD)");
 static const char MAINMENUENTRY[] = trNOOP("Signal Information");
 
