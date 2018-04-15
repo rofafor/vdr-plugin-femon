d366856c719874ddf13886a00d741c4faa14130c
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2018-04-15 22:04:48 +0300

    Updated for vdr-2.4.0.

--- femon.c
+++ femon.c
@@ -17,15 +17,15 @@
 #include "tools.h"
 #include "setup.h"
 
-#if defined(APIVERSNUM) && APIVERSNUM < 20307
-#error "VDR-2.3.7 API version or greater is required!"
+#if defined(APIVERSNUM) && APIVERSNUM < 20400
+#error "VDR-2.4.0 API version or greater is required!"
 #endif
 
 #ifndef GITVERSION
 #define GITVERSION ""
 #endif
 
-static const char VERSION[]       = "2.3.0" GITVERSION;
+static const char VERSION[]       = "2.4.0" GITVERSION;
 static const char DESCRIPTION[]   = trNOOP("DVB Signal Information Monitor (OSD)");
 static const char MAINMENUENTRY[] = trNOOP("Signal Information");
 
